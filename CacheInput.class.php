eef19ea5ddad472fbde89b13d744f38e406c65ff
folk_zombie <maddDidley@charter.net>
2013-04-12 23:48:45 -0500

    first commit

---
+++ CacheInput.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class CacheInput {
+
+	private $the_file;
+	public $input;
+	public $cache_timeout;
+	
+	public function __construct($the_file, $input='', $cache_timeout=0) {          
+		$this->the_file = $the_file;
+		$this->input = $input; 
+		$this->cache_timelimit = $cache_timeout;
+	}
+	
+	///////////READ THE CACHE FILE
+	public function readCache() {
+		$cacheFile = $this->the_file;
+		$handle = fopen($cacheFile, "r");
+		$contents = fread($handle, filesize($cacheFile));
+		return $contents;
+	}
+	
+	///////////GENERATE THE CACHE FILE
+	public function generateCache() {
+		if (!$handle = fopen($this->the_file, 'w')) { return false; } //Cannot open file
+		if (fwrite($handle, $this->input) === FALSE) { return false; } //Cannot write to file
+		fclose($handle);
+		return true;
+	}
+	
+	///////////CHECK THE CACHE FILE
+	public function checkCache() {
+		$time_created = @filemtime($this->the_file);
+		$diff = time() - $this->cache_timeout; // check and see if file is older than a half a hour
+		if($time_created > $diff) {
+			return false; // use cache file
+		} else {
+			return true; // generate new file
+		}
+	}
+	
+}
+
+?>
